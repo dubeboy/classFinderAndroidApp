9414dd8e94acb64658c6050f6213378d42a8b819
Divine <dubedivine@gmail.com>
2017-07-04 19:52:46 +0200

    add notfications and paying destination

---
+++ app/src/main/java/za/co/metalojiq/classfinder/someapp/activity/HostDashboard.kt
@@ -0,0 +1,14 @@
+package za.co.metalojiq.classfinder.someapp.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import za.co.metalojiq.classfinder.someapp.R
+
+class HostDashboard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_host_dashboard)
+    }
+}
